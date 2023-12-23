bab0d80d8f3175217d125ec6f969ca900580671f
Lucas Fernandes <142260544+LepoFernandes@users.noreply.github.com>
2023-12-23 03:39:56 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     var btnLogin = document.getElementById("login-btn");
     var btnCadastro = document.getElementById("cadastro");
     var body = document.querySelector("body");
 
-    btnLogin.addEventListener("click", function () {
-    body.className = "sign-in-js";
+    btnLogin.addEventListener("click", function() {
+        body.className = "sign-in-js";
     });
 
-    btnCadastro.addEventListener("click", function () {
-    body.className = "sign-up-js";
+    btnCadastro.addEventListener("click", function() {
+        body.className = "sign-up-js";
     });
 
+ 
+    var btnNormalLogin = document.getElementById("buttonnormal");
+    btnNormalLogin.addEventListener("click", function() {
+        
+        console.log("Botão de login normal clicado");
+    });
 });
-
